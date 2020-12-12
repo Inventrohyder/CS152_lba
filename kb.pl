f7d641f7df0f5bea10f56cb25827c40135e87d8f
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-12-12 04:18:38 +0100

    make it work Eureka

--- kb.pl
+++ kb.pl
@@ -5,3 +5,15 @@ telldisease(malaria,sweating,sweating,sweating,sweating).
 telldisease(fever,X,sweating,sweating,sweating).
 
 telldisease(fever,X,sweating,sweating,sweating).
+
+%RULES
+
+
+telldisease(malaria,'I am currently experiencing COVID symptoms, I want to know what to do',res,turk).
+
+
+%rules for I HAVE SYMPTOMS
+
+answer('You need to test at City Test SF','I am currently experiencing COVID symptoms, I want to know what to do',res,turk).
+
+
--- test.py
+++ test.py
@@ -5,9 +5,25 @@ from PIL import ImageTk, Image
 from pyswip import *
 
 
-sympList = ['--Select Symptom--', 'headache', 'sneezing', 'runny_nose', 'sore_throat', 'fever', 'chills', 'bodyache',
+sympList = ['--Select--', 'headache', 'sneezing', 'runny_nose', 'sore_throat', 'fever', 'chills', 'bodyache',
             'abdominal_pain', 'loss_of_appetite', 'skin_rash', 'conjunctivitus', 'sweating', 'vomitting', 'diarrhea']
 
+
+intention = [
+    'I am currently experiencing COVID symptoms, I want to know what to do',
+    'I want to travel around',
+    'I had a close contact with a COVID patient. What next?',
+    'I just wanted to know about some casual stuff'
+]
+
+reside = [
+    "res",
+]
+
+halls = [
+    'turk',
+    '851'
+]
 
 class DPDP:
 
@@ -31,38 +47,38 @@ class DPDP:
         # ----------symptoms selection---------------------------------------------------
 
         # -------Symptom1-------------------------
-        symp1 = Label(root, text="1st Symptom",
+        symp1 = Label(root, text="Intention",
                       font=('Verdana 15'), bg='#44689E')
         symp1.place(x=20, y=200)
 
         self.selSymp1 = StringVar()
         self.selSymp1.set(sympList[0])
 
-        sympDropDown1 = OptionMenu(root, self.selSymp1, *sympList)
+        sympDropDown1 = OptionMenu(root, self.selSymp1, *intention)
         sympDropDown1.place(x=180, y=200)
 
         # -------Symptom2-------------------------
 
-        self.symp2 = Label(root, text="2nd Symptom",
+        self.symp2 = Label(root, text="Res",
                            font=('Verdana 15'), bg='#44689E')
         self.symp2.place(x=20, y=300)
 
         self.selSymp2 = StringVar()
         self.selSymp2.set(sympList[0])
 
-        sympDropDown2 = OptionMenu(root, self.selSymp2, *sympList)
+        sympDropDown2 = OptionMenu(root, self.selSymp2, *reside)
         sympDropDown2.place(x=180, y=300)
 
         # -------Symptom3-------------------------
 
-        self.symp3 = Label(root, text="3rd Symptom",
+        self.symp3 = Label(root, text="halls",
                            font=('Verdana 15'), bg='#44689E')
         self.symp3.place(x=20, y=400)
 
         self.selSymp3 = StringVar()
         self.selSymp3.set(sympList[0])
 
-        sympDropDown3 = OptionMenu(root, self.selSymp3, *sympList)
+        sympDropDown3 = OptionMenu(root, self.selSymp3, *halls)
         sympDropDown3.place(x=180, y=400)
 
         # -------Symptom4-------------------------
@@ -89,12 +105,17 @@ def queryGenerator(s1, s2, s3, s4):
     prolog = Prolog()
     prolog.consult('kb.pl')
 
-    q = list(prolog.query("telldisease(X,%s,%s,%s,%s)." % (s1, s2, s3, s4)))  # prolog query
+    q = list(prolog.query("answer(X, '%s',%s,%s)." % (s1, s2, s3)))  # prolog query
    # for e in q[0].values():
    # print("You have " + e)
    # break
     print(q)
-    tkinter.messagebox.showinfo(title=str(q), message=str(q))
+    for v in q:
+        print(v)
+        print(v['X'])
+        tkinter.messagebox.showinfo(title=str(v), message=str(v['X']))
+    
+    
 
 
 root = Tk()
